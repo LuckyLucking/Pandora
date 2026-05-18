fa2e063f20cd2ddc70b21b014a409daaac95106a
LuckyLucking <2601258846@qq.com>
2026-05-18 13:47:01 +0800

    GeneratorFinished

--- Assets/Scripts/2D/PandoraGenerator.cs
+++ Assets/Scripts/2D/PandoraGenerator.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PandoraGenerator : MonoBehaviour
 {
-    public int[,] pandoraMap;
     public bool[,] isTaken;
 
     [Header("Blocks")]
@@ -15,4 +14,81 @@ public class PandoraGenerator : MonoBehaviour
     public GameObject meatSp;
     public GameObject treeSp;
 
+    [Header("GenerateChance")]
+    public float treeChance = 10;
+    public float grassChance = 20;
+    public float meatChance = 10;
+
+    [Header("Resources")]
+    public int grassAmount = 0;
+    public int meatAmount = 0;
+
+    private void Start()
+    {
+        isTaken = new bool[width, height];
+
+        GenerateTerrain();
+    }
+    private void GenerateTerrain()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                InitMap(x, y);
+            }
+        }
+    }
+    private void InitMap(int x,int y)
+    {
+        GenerateBorder(x, y);
+        int tChance = Random.Range(0, 99);
+
+        if (tChance <= treeChance && isTaken[x, y] == false)
+            GenerateTree(x, y);
+
+        if (isTaken[x, y] == false)
+            GenerateGround(x, y);
+
+        int gChance = Random.Range(0, 99);
+
+        if (gChance <= grassChance && isTaken[x, y] == false)
+            GenerateGrass(x, y);
+
+        int mChance = Random.Range(0, 99);
+        if(mChance <= meatChance && isTaken[x, y] == false)
+            GenerateMeat(x, y);
+    }
+    private void GenerateBorder(int x, int y)
+    {
+        if(x == 0)
+            Instantiate(treeSp, new Vector3(x -0.5f,y+0.5f ), treeSp.transform.rotation, transform);
+        if (x==width -1)
+            Instantiate(treeSp, new Vector3(x + 1.5f,y+0.5f), treeSp.transform.rotation, transform);
+        if (y == 0)
+            Instantiate(treeSp, new Vector3(x+0.5f,y -0.5f), treeSp.transform.rotation, transform);
+        if (y==height -1)
+            Instantiate(treeSp, new Vector3(x +0.5f,y + 1.5f), treeSp.transform.rotation, transform);
+
+    }
+    private void GenerateGround(int x, int y)
+    {
+        GameObject ground = Instantiate(groundSp, new Vector3(x + 0.5f,  y + 0.5f), groundSp.transform.rotation, transform);
+
+    }
+    private void GenerateTree(int x, int y)
+    {
+        GameObject tree = Instantiate(treeSp,new Vector3(x +0.5f,y+0.5f),treeSp.transform.rotation,transform);
+        isTaken[x, y] = true;
+    }
+    private void GenerateGrass(int x, int y)
+    {
+        GameObject grass = Instantiate(grassSp, new Vector3(x + 0.5f, y + 0.5f), grassSp.transform.rotation, transform);
+        isTaken[x, y] = true;
+    }   
+    private void GenerateMeat(int x, int y)
+    {
+        GameObject meat = Instantiate(meatSp, new Vector3(x + 0.5f, y + 0.5f), meatSp.transform.rotation, transform);
+        isTaken[x, y] = true;
+    }
 }
